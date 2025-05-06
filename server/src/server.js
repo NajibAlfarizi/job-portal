96c8c917a9e5b0680bc71749a962dd03830e9bd7
Najib Alfarizi <najibalfarizi5@gmail.com>
2025-05-06 23:44:13 +0700

    Initialize server with Expresss

---
+++ server/src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+//server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
